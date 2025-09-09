19ed91b31b0a62321ae2f45a9bacb6bd7516365d
Hyeongyu Son <shonenq00@gmail.com>
2025-09-09 14:21:14 +0900

    .

--- src/main/java/com/findex/repository/IndexInfoRepository.java
+++ src/main/java/com/findex/repository/IndexInfoRepository.java
@@ -1,7 +1,11 @@
 package com.findex.repository;
 
 import com.findex.entity.IndexInfo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long> {
+
+  List<IndexInfo> findAllByFavoriteIsTrue();
+  List<IndexInfo> findAllByFavorite(boolean favorite);
 }
