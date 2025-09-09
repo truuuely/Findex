d056e91031a9984097b6aef606cbdb6486b74bd5
BakJiSeok <commicat2@gmail.com>
2025-09-09 11:20:58 +0900

    feat: IndexInfoSummary 검색을 위한 엔드포인트 추가

--- src/main/java/com/findex/repository/IndexInfoRepository.java
+++ src/main/java/com/findex/repository/IndexInfoRepository.java
@@ -1,10 +1,21 @@
 package com.findex.repository;
 
+import com.findex.dto.indexinfo.IndexInfoSummaryDto;
 import com.findex.entity.IndexInfo;
 import com.findex.exception.NotFoundException;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long> {
+
+    @Query("""
+        SELECT new com.findex.dto.indexinfo.IndexInfoSummaryDto(
+            i.id, i.indexClassification, i.indexName
+        )
+        FROM IndexInfo i
+    """)
+    List<IndexInfoSummaryDto> findAllSummaries();
 
     default IndexInfo getOrThrow(Long id) {
         return findById(id).orElseThrow(() ->
