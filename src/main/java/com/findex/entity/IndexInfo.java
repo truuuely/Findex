ce355b612436a8a2314118f0028232e3bbf6755c
BakJiSeok <commicat2@gmail.com>
2025-09-09 16:23:19 +0900

    chore: setter 필요 필드에만 부착

--- src/main/java/com/findex/entity/IndexInfo.java
+++ src/main/java/com/findex/entity/IndexInfo.java
@@ -6,16 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
@@ -26,14 +24,19 @@ public class IndexInfo extends BaseEntity {
 
     private String indexName;
 
+    @Setter
     private Integer employedItemsCount;
 
+    @Setter
     private LocalDate basePointInTime;
 
+    @Setter
     private Integer baseIndex;
 
+    @Setter
     @Enumerated(EnumType.STRING)
     private IndexSourceType sourceType;
 
+    @Setter
     private boolean favorite;
 }
